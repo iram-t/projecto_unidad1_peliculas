ac8cddc56480b2171d8b28fd64695effe59fa6b7
Iram Terrazas <iramgustavo235@gmail.com>
2023-09-13 12:28:23 -0600

    Card swiper and images

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,34 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // pa guardar el tamaño
+    final size = MediaQuery.of(context).size;
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.red,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.4,
+        itemBuilder: (_, index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(
+              context,
+              'details',
+              arguments: '',
+            ),
+            // Para esquinas redondeadas
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/imagen.jpg'),
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
