18851b25086b95b7da2ed60c709b3395f9c6c900
Iram Terrazas <iramgustavo235@gmail.com>
2023-09-28 11:50:49 -0600

    provider, movieprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:projecto_unidad1/screens/screems.dart';
+import 'package:provider/provider.dart';
+import 'providers/movies_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          //Que se cree en cuanto se cree la aplicacion
+          lazy: false,
+        )
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
